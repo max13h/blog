f0a4330138605f12710108ac599ad49e11433599
mhmae <mhmae@unsitra.fr>
2026-01-26 12:56:20 +0100

    🩹 Remove useless "base" in astro.config.mjs & rename BaseLayout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.hmae.fr/",
-  base: "/",
-  trailingSlash: "ignore",
+  site: "https://blog.hmae.fr",
 })
