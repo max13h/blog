d0a72f9f4a5ec73da82ca3a0eb0ea55aa48e224f
mhmae <mhmae@unsitra.fr>
2026-01-26 11:23:16 +0100

    🔧 Update astro site & base config accordingly to github page domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://max13h.github.io",
-  base: "/blog",
+  site: "https://blog.hmae.fr/",
+  base: "/",
+  trailingSlash: "ignore",
 })
