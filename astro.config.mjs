b72fb89991a07a11a99c31ac5d0a289f29978771
mhmae <mhmae@unsitra.fr>
2026-02-03 10:48:20 +0100

    📦️ Add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from "astro/config"
 
 import vue from "@astrojs/vue"
 
+import icon from "astro-icon"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.hmae.fr",
-  integrations: [vue()],
+  integrations: [
+    vue(),
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+  ],
 })
